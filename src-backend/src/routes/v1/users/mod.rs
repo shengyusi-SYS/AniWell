9eeb9db20d40ec47ce0072a1c6f1c7b636e6fe56
生于斯 <38646996+blytzxdl@users.noreply.github.com>
2024-06-10 22:00:12 +0800

    save

--- src-backend/src/routes/v1/users/mod.rs
+++ src-backend/src/routes/v1/users/mod.rs
@@ -1,13 +1,27 @@
-use anyhow::Ok;
 use axum::{
+    extract,
+    http::StatusCode,
     routing::{get, post},
     Router,
 };
+use serde::Deserialize;
 
-use crate::structs::ServerError;
+struct User {}
 
+#[derive(Debug, Deserialize)]
+pub struct LoginData {
+    username: String,
+    password: String,
+}
 pub fn route() -> Router {
     Router::new().route("/login", get(login))
 }
 
-async fn login() -> Result<(), ServerError> {}
+async fn login(extract::Json(data): extract::Json<LoginData>) -> StatusCode {
+    println!("{:?}", data);
+    if data.username == "admin" && data.password == "admin" {
+        StatusCode::OK
+    } else {
+        StatusCode::UNAUTHORIZED
+    }
+}
